595c623313906af44bb77f2e2467a1e56196277b
John Mitchell <neufena@minusmoney.co.uk>
2023-07-21 22:30:11 +0100

    update readme and add logging

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # HOONkbox Vision control software
 
-### Midi Notes:
+## Midi Notes
 
 - 1: All off (stops smoke and all videos)
 - 2: Smoke (velocity = duration in deciseconds)
-- 10: Video - play once (velocity file suffix videoX.h264)
-- 11: Video - play looped (velocity file suffix videoX.h264)
+- 10: Video - play once (velocity X file suffix IE videoX.h264)
+- 11: Video - play looped (velocity X file suffix IE videoX.h264)
 - 12: Video - stop
 
-### To encode video to correct format:
+## To encode video to correct format
 
 ffmpeg -i input_file.avi -vcodec copy -an -bsf:v h264_mp4toannexb output_file.h264
 
--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,93 @@
-const rtpmidi = require('rtpmidi');
-const rpio = require('rpio');
-const { spawn, spawnSync } = require('child_process');
+const rtpmidi = require('rtpmidi'); // eslint-disable-line @typescript-eslint/no-var-requires
+const rpio = require('rpio'); // eslint-disable-line @typescript-eslint/no-var-requires
+import { spawn, spawnSync } from 'child_process';
 
 const remote = '192.168.10.10';
 const smokePin = 11;
 const videoPath = '/home/pi/videos';
 const playerPath = '/home/pi/bin/hello_video.bin';
+const verbose = false;
 
 rpio.open(smokePin, rpio.OUTPUT, rpio.HIGH);
 rtpmidi.logger.transports[0].level = 'error';
 
 const session = rtpmidi.manager.createSession({
-  name: 'HOONkbox Vision',
-  port: 5004,
+    name: 'HOONkbox Vision',
+    port: 5004,
 });
 
 session.connect({ address: remote, port: 5004 });
 
 session.on('message', (deltaTime: number, message: Buffer) => {
-  const commands: number[] = Array.prototype.slice.call(message, 0);
-  if (commands[0] !== 144) {
-    return;
-  }
+    const commands: number[] = Array.prototype.slice.call(message, 0);
+    if (commands[0] !== 144) {
+        return;
+    }
 
-  const note: number = commands[1];
-  const velocity: number = commands[2];
+    const note: number = commands[1];
+    const velocity: number = commands[2];
 
-  if (note === 1) {
-    allOff();
-    return;
-  }
+    if (note === 1) {
+        allOff();
+        return;
+    }
 
-  if (note === 2) {
-    smoke(velocity * 100);
-    return;
-  }
+    if (note === 2) {
+        smoke(velocity * 100);
+        return;
+    }
 
-  if (note === 10) {
-    videoPlay(velocity, false);
-    return;
-  }
+    if (note === 10) {
+        videoPlay(velocity, false);
+        return;
+    }
 
-  if (note === 11) {
-    videoPlay(velocity, true);
-    return;
-  }
+    if (note === 11) {
+        videoPlay(velocity, true);
+        return;
+    }
 
-  if (note === 12) {
-    videoStop();
-    return;
-  }
+    if (note === 12) {
+        videoStop();
+        return;
+    }
 });
 
 function allOff() {
-  smokeOff();
-  videoStop();
+    smokeOff();
+    videoStop();
 }
 
 function smokeOn() {
-  rpio.write(smokePin, rpio.LOW);
+    rpio.write(smokePin, rpio.LOW);
 }
 
 function smokeOff() {
-  rpio.write(smokePin, rpio.HIGH);
+    log('Smoke off');
+    rpio.write(smokePin, rpio.HIGH);
 }
 
 function smoke(duration: number) {
-  smokeOn();
-  setTimeout(smokeOff, duration);
+    log('Smoke on for' + duration / 10 + ' seconds');
+    smokeOn();
+    setTimeout(smokeOff, duration);
 }
 
 function videoPlay(file: number, loop: boolean) {
-  const path = videoPath + '/video' + file + '.h264';
-  const loopArg = '--loop' + (loop ? '' : '=1');
-  videoStop();
-  spawn(playerPath, [loopArg, path]);
+    const path = videoPath + '/video' + file + '.h264';
+    const loopArg = '--loop' + (loop ? '' : '=1');
+    videoStop();
+    log('Play video' + path + '.h264 ' + (loop ? 'looped' : 'once'));
+    spawn(playerPath, [loopArg, path]);
 }
 
 function videoStop() {
-  spawnSync('killall', ['hello_video.bin']);
+    log('Stop videos');
+    spawnSync('killall', ['hello_video.bin']);
+}
+
+function log(message: string) {
+    if (verbose) {
+        console.log(message);
+    }
 }
